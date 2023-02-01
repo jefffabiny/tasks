2ed8e8b7d41a77b82265af70758e9691e1c4a756
Jeff Fabiny <jefffabiny@gmail.com>
2023-01-31 19:00:09 -0500

    Somewhat reasonable state.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { TasksContainerComponent } from './tasks-container/tasks-container.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, TasksContainerComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [MatIconModule],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/tasks-container/tasks-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks-container',
+  templateUrl: './tasks-container.component.html',
+  styleUrls: ['./tasks-container.component.scss'],
+})
+export class TasksContainerComponent {
+  emptySet = true;
+}
